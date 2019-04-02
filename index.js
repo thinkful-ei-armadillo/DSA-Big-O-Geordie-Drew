37fb33bb5c6aed8d72caed93608da0c218c7a3c7
Geordie Connell <geordie.connell@gmail.com>
2019-04-02 14:43:13 -0400

    got to #12, switching

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-function towerOfHanoi(disc, src, aux, dst){
+function towerOfHanoi(disc, src, aux, dst, move = 0){
     if(disc > 0){
-        towerOfHanoi(disc - 1, src, dst, aux);
-        console.log("Move disc " + disc + " from " + src + " to " + dst)
-        towerOfHanoi(disc - 1, aux, src, dst);
+        towerOfHanoi(disc - 1, src, dst, aux, move);
+        console.log("Move #: " +move+ " disc " + disc + " from " + src + " to " + dst);
+        move++;
+        towerOfHanoi(disc - 1, aux, src, dst, move);
     }
 }
 
-towerOfHanoi(5, "src", "aux", "dst");
+// towerOfHanoi(3, "src", "aux", "dst");
+
+//#12-1 Counting Sheep drill
+function countingSheep(n) {
+    for (let i = 0; i < n+1; i++) {
+        if (i < n) {
+            console.log(`${n-i}: Another sheep jumps over the fence`);
+        } else {
+            console.log('All sheep have jumped over the fence!')
+        }
+    }
+}
+
+// countingSheep(3);
+
+//#12-2 Power Calc drill
+function powerCalculator(base, exp) {
+    let product = 1;
+    for(let i = 0; i < exp; i++) {
+        product *= base;
+    }
+    return product;
+}
+
+// console.log(powerCalculator(2, 4));
+// console.log(powerCalculator(10, 2));
