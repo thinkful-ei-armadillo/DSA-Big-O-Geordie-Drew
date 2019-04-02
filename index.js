dd319b49ec47a5da896058a62cb6d434c8276b73
andrewbituin <fandrewbituin@gmail.com>
2019-04-02 12:23:41 -0700

    completed iterative solutions to recursive problems

--- index.js
+++ index.js
@@ -33,3 +33,62 @@ function powerCalculator(base, exp) {
 
 // console.log(powerCalculator(2, 4));
 // console.log(powerCalculator(10, 2));
+
+function reverseString(str){
+    let reversedStr = '';
+    for(let i = str.length - 1; i >= 0; i--){
+        reversedStr += str.slice(i, i + 1)
+    }
+    return reversedStr;
+}
+// console.log(reverseString('hello'));
+
+function triangularNumber(n){
+    let sum = 0;
+    for(let i = 1; i < n + 1; i++){
+        sum += i;
+    }
+    return sum;
+}
+// console.log(triangularNumber(3))
+
+function stringSplitter(str, separator){
+    let splitString = '';
+    for(let i = 0; i < str.length; i++){
+        if(str[i] !== separator){
+            splitString += str.slice(i, i + 1);
+        }
+    }
+    return splitString;
+}
+
+// console.log(stringSplitter('02/20/2020', '/'))
+
+function fib(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+
+        if (i === 1) {
+            result.push(0);
+        }
+        else if (i == 2) {
+            result.push(1);
+        }
+        else {
+            result.push(result[i - 2] + result[i - 3]);
+        }
+    }
+    return result.pop();
+}
+
+// console.log(fib(7))
+
+function factorial(num){
+    let result = 1;
+    for(let i = 1; i < num + 1; i++){
+        result *= i;
+    }
+    return result;
+}
+
+// console.log(factorial(5))
